36321701e2f28cfea3e785bc526191fc04d33b54
Satya Karanam <karanam.s@indusnet.co.in>
2016-10-26 18:40:09 +0530

    /*check one

--- AppletsExample/src/com/applet/test.java
+++ AppletsExample/src/com/applet/test.java
@@ -5,6 +5,7 @@ public class test
 	public static void main(String args[])
 	 {
 		System.out.println("satya ");
+		System.out.println("github");
 			int i = 1;
 			i = i >> 1;	 
 	 		System.out.println(i);
